a22679dee884c720e09c2f7bc085a7b0218301c4
IvEugen <eugen-iv@mail.ru>
2022-11-15 01:49:01 +0500

    added main

--- shortener.go
+++ shortener.go
@@ -2,17 +2,7 @@ package shortener
 
 import (
 	"math/rand"
-
-	"go.k6.io/k6/js/modules"
 )
-
-const version = "v0.0.1"
-
-func init() {
-	modules.Register("shortener", &Short{
-		Version: version,
-	})
-}
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
 
