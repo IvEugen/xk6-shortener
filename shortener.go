40113871e138c4662aa9b529b2aee4e9325e3aa8
IvEugen <eugen-iv@mail.ru>
2022-11-15 01:27:10 +0500

    added version

--- shortener.go
+++ shortener.go
@@ -6,13 +6,19 @@ import (
 	"go.k6.io/k6/js/modules"
 )
 
+const version = "v0.0.1"
+
 func init() {
-	modules.Register("k6/x/shortener", new(Short))
+	modules.Register("k6/x/shortener", &Short{
+		Version: version,
+	})
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
 
-type Short struct{}
+type Short struct {
+	Version string
+}
 
 func (*Short) GeneratorURL(n int) string {
 	b := make([]rune, n)
